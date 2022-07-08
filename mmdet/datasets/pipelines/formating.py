2efc70dc08cc1727192bcbd480ef137edf70994c
LuZhengy-e <ddlzy@hnu.edu.cn>
2022-07-08 15:51:27 +0800

    add twoimage case

--- mmdet/datasets/pipelines/formating.py
+++ mmdet/datasets/pipelines/formating.py
@@ -276,6 +276,61 @@ class DefaultFormatBundle:
 
 
 @PIPELINES.register_module()
+class TwoImageFormatBundle(DefaultFormatBundle):
+    def __call__(self, results):
+        """Call function to transform and format common fields in results.
+
+        Args:
+            results (dict): Result dict contains the data to convert.
+
+        Returns:
+            dict: The result dict contains the data that is formatted with 
+                default bundle.
+        """
+        if 'img' in results:
+            img = results['img']
+            if self.img_to_float is True and img.dtype == np.uint8:
+                # Normally, image is of uint8 type without normalization.
+                # At this time, it needs to be forced to be converted to
+                # flot32, otherwise the model training and inference
+                # will be wrong. Only used for YOLOX currently .
+                img = img.astype(np.float32)
+            # add default meta keys
+            results = self._add_default_meta_keys(results)
+            if len(img.shape) < 3:
+                img = np.expand_dims(img, -1)
+            img = np.ascontiguousarray(img.transpose(2, 0, 1))
+            results['img'] = DC(
+                to_tensor(img), padding_value=self.pad_val['img'], stack=True)
+        if "traj" in results:
+            traj = results["traj"]
+            if self.img_to_float is True and traj.dtype == np.uint8:
+                traj = traj.astype(np.float32)
+            
+            if len(traj.shape) < 3:
+                traj = np.expand_dims(img, -1)
+            traj = np.ascontiguousarray(traj.transpose(2, 0, 1))
+            results['traj'] = DC(
+                to_tensor(traj), padding_value=self.pad_val['img'], stack=True)
+
+        for key in ['proposals', 'gt_bboxes', 'gt_bboxes_ignore', 'gt_labels']:
+            if key not in results:
+                continue
+            results[key] = DC(to_tensor(results[key]))
+        if 'gt_masks' in results:
+            results['gt_masks'] = DC(
+                results['gt_masks'],
+                padding_value=self.pad_val['masks'],
+                cpu_only=True)
+        if 'gt_semantic_seg' in results:
+            results['gt_semantic_seg'] = DC(
+                to_tensor(results['gt_semantic_seg'][None, ...]),
+                padding_value=self.pad_val['seg'],
+                stack=True)
+        return results
+
+
+@PIPELINES.register_module()
 class Collect:
     """Collect data from the loader relevant to the specific task.
 
